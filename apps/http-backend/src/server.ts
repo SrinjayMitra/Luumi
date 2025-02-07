01539fd93ee87ed77244f88ad7b4be9f73c19d3a
SrinjayMitra <srinjaymitra918@gmail.com>
2025-02-07 05:25:52 -0800

    added db

--- apps/http-backend/src/server.ts
+++ apps/http-backend/src/server.ts
@@ -1,14 +1,24 @@
 import express  from "express";
 import cors from "cors";
+import { prismaClient } from "@repo/db/PrismaClient";
 
 const app = express(); 
 const port = 3001;
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/hello/', (req, res) => {
+app.get('/api/hello/', async (req, res) => {
+  const db = await prismaClient.user.create({
+      data:{
+        name:"n",
+        email:"n@example.com",
+        password:"n",
+      }
+  })
+
     res.json({
         mssg:"hello"})});
+
 app.listen(port,()=>{
     console.log("Service listening on port " + port);
 });
